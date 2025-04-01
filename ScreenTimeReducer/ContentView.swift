d3382cad15f02e753aff0a90fa92c880cbd4e28b
Tanaka Yuki <tnkyk0126@gmail.com>
2025-04-01 23:24:49 +0900

    Initial Commit

---
+++ ScreenTimeReducer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScreenTimeReducer
+//
+//  Created by yuki on R 7/04/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScreenTimeReducer/ScreenTimeReducerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenTimeReducerApp.swift
+//  ScreenTimeReducer
+//
+//  Created by yuki on R 7/04/01.
+//
+
+import SwiftUI
+
+@main
+struct ScreenTimeReducerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
